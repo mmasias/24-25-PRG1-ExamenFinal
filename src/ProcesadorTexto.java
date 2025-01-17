3f8f5bfe007b2678cb1ac5abb1685dc7e3059fc0
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2025-01-17 12:48:14 +0100

    Con la ayuda del if podemos acortar los resultados directamente en un solo if sin dnecesidad de poner 2 if

--- src/ProcesadorTexto.java
+++ src/ProcesadorTexto.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class ProcesadorTexto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Introduce un texto:");
         String texto = sc.nextLine();
         String resultado = "";
@@ -15,7 +15,7 @@ class ProcesadorTexto {
             if (c == ' ' || c == '.' || c == ',' || c == ';') {
                 if (enPalabra) {
                     palabras++;
-                    enPalabra = false;º
+                    enPalabra = false;
                 }
                 if (c != ' ') {
                     resultado += c + " ";
